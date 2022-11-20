22e56f64a250ee33409fc3e25bbff1c15587b11a
zakaria <zakaria@ymail.com>
2022-11-20 04:19:04 +0200

    strong padding

--- code.js
+++ code.js
@@ -14,7 +14,7 @@ hamburger.addEventListener("click", () => {
 
 document.addEventListener("scroll", () => {
   var scroll_position = window.scrollY;
-  if (scroll_position > 250) {
+  if (scroll_position > 200) {
     header.style.backgroundColor = "#29323c";
   } else {
     header.style.backgroundColor = "transparent";
